a0a4eff1828a31662e5a6ae395186cd1c5d1c8b7
JGardiner99 <59534902+JGardiner99@users.noreply.github.com>
2020-01-13 20:39:57 +0000

    update

--- empty-example/indexSketch.js
+++ empty-example/indexSketch.js
@@ -2,7 +2,9 @@ var theme;
 var gameFont;
 var success;
 var fail;
-
+var volumeButtonUnmute;
+var volumeButtonMute;
+var showImage;
 let stars = [];
 let speed;
 
@@ -15,100 +17,88 @@ var inter = 60;
 var minutes = Math.floor(timer / inter);
 var seconds = timer % inter;
 
-var volumeButton;
-var muteButton;
 function preload()
 {
-	theme = loadSound("theme3.ogg");
+	theme = loadSound("./Sounds/theme3.ogg");
 	gameFont = loadFont("PressStart2P-Regular.ttf");
-	success = loadSound("click success.wav");
-	fail = loadSound("click fail.wav");
-	Unute = loadImage ("ButtonVolumeMute.png");
-	Mute = loadImage ("ButtonVolumeOnLayered.png");
+	success = loadSound("./Sounds/click success.wav");
+	fail = loadSound("./Sounds/click fail.wav");
+	img1 = loadImage("./Images/volumeButton1.png");
+	img2 = loadImage("./Images/volumeButton2.png");
 }
 
 function setup()
 {
-    document.body.style.overflow = 'hidden';
-    createCanvas(windowWidth, windowHeight);
+	document.body.style.overflow = 'hidden';
+	createCanvas(windowWidth, windowHeight);
 
-	theme.setVolume(0.25);
+	theme.setVolume(0.15);
+	success.setVolume(0.5);
+	fail.setVolume(0.75);
 	theme.loop();
 
-    function Star()
-    {
-      this.x = random(-width, width);
-      this.y = random(-height, height);
-      this.z = random(width);
+	function Star()
+	{
+		this.x = random(-width, width);
+		this.y = random(-height, height);
+		this.z = random(width);
 
-      this.display = function()
-      {
-        noStroke();
-        fill(255);
-        let sx = map(this.x / this.z, 0, 1, 0, width/2);
-        let sy = map(this.y / this.z, 0, 1, 0, height/2);
-        let r = map(this.z, 0, width, 12, 0);
-        ellipse(sx, sy, r, r);
-      } // display
+		this.display = function()
+		{
 
-      this.update = function()
-      {
-        this.z -= speed;
+		noStroke();
+		fill(255);
+		let sx = map(this.x / this.z, 0, 1, 0, width/2);
+		let sy = map(this.y / this.z, 0, 1, 0, height/2);
+		let r = map(this.z, 0, width, 12, 0);
+		ellipse(sx, sy, r, r);
+		} // display
 
-        if(this.z < 1)
-        {
-          this.z = width;
-          this.x = random(-width, width);
-          this.y = random(-height, height);
-        }
-      } // update
-    } // Star
+		this.update = function()
+		{
+			this.z -= speed;
 
-    for(let i = 0; i < 500; i += 1)
-    {
-      stars[i] = new Star();
-    }
-	Mute.resize(100, 100);
-    ball = new Ball();
+			if(this.z < 1)
+			{
+				this.z = width;
+				this.x = random(-width, width);
+				this.y = random(-height, height);
+			}
+		} // update
+	} // Star
+
+	for(let i = 0; i < 500; i += 1)
+	{
+	stars[i] = new Star();
+	}
+	img1.resize(100, 100);
+	img2.resize(100, 100);
+	ball = new Ball();
 }
 
 function draw()
 {
-  background(0);
+	background(0);
 
-  textFont(gameFont);
-  translate(width/2, height/2);
-  speed = map(timeElapsed * 50, 0, width, 2, 20);
+	textFont(gameFont);
+	translate(width/2, height/2);
+	speed = map(timeElapsed * 50, 0, width, 2, 20);
 
-  for(let i = 0; i < stars.length; i += 1)
-  {
-    stars[i].display();
-    stars[i].update();
-  }
-  translate(-width/2, -height/2);
-	fill(239, 222, 136);
-	textSize(20);
-	stroke(0);
-	strokeWeight(4);
-	text("Score: " + score, 10, 30);
-	text("Misses Left: " + miss, 10, 60);
-	if (seconds > 9)
+	for(let i = 0; i < stars.length; i += 1)
 	{
-		text("Time Remaining: " + minutes + ":" + seconds, width -405, 30);
+		stars[i].display();
+		stars[i].update();
 	}
-	else
-	{
-		text("Time Remaining: " + minutes + ":0" + seconds, -405, 30);
-	}
-	noStroke();
+
+	translate(-width/2, -height/2);
+
 	if(frameCount % 60 == 0 && timer > 0)
 	{
 		timer--;
 		minutes = Math.floor(timer / inter);
 		seconds = timer % inter;
 
-    timeElapsed += 1;
-	console.log(timeElapsed);
+		timeElapsed += 1;
 	}
 
 
@@ -116,11 +106,43 @@ function draw()
 	{
 		window.location.replace("menu.html");
 	}
-	
-	image(Mute, width - 100, height - 100);
-		ball.update();
-    ball.edges();
-    ball.show();
+
+	ball.update();
+	ball.edges();
+	ball.show();
+
+		fill(239, 222, 136);
+		textSize(20);
+		stroke(0);
+		strokeWeight(4);
+		text("Score: " + score, 10, 30);
+		text("Misses Left: " + miss, 10, 60);
+
+		if (seconds > 9)
+		{
+			text("Time Remaining: " + minutes + ":" + seconds, width -405, 30);
+		}
+		else
+		{
+			text("Time Remaining: " + minutes + ":0" + seconds, -405, 30);
+		}
+			noStroke();
+
+		image(volumeButtonMute, width, height);
+}
+
+function imageUnmute()
+{
+	if(isMuted = false)
+	showImage = img1;
+}
+
+function imageMute()
+{
+	if(isMuted = true)
+	{
+		showImage = img2;
+	}
 }
 
 function windowResized()
@@ -136,46 +158,45 @@ function mousePressed()
 //ball class
 class Ball
 {
-    constructor()
+	constructor()
 	{
-        this.x = width/2;
-        this.y = height/2;
-        this.xspeed = 0;
-        this.yspeed = 0;
-        this.xspeedbool = 0;
+		this.x = width/2;
+		this.y = height/2;
+		this.xspeed = 0;
+		this.yspeed = 0;
+		this.xspeedbool = 0;
 		this.yspeedbool = 0;
 		this.r = 50
-		this. isMuted = new Boolean(false);
-        this.reset();
-    }
+		this.isMuted = new Boolean(false);
+		this.reset();
+	}
 
-    update()
+	update()
 	{
-        this.x += this.xspeed;
-        this.y += this.yspeed;
-    }
+		this.x += this.xspeed;
+		this.y += this.yspeed;
+	}
 
-    reset()
+	reset()
 	{
-        this.x = width/2;
-        this.y = height/2;
-        let angle = random(-PI/4, PI/4);
-        this.xspeed = 5 * Math.cos(angle);
-        this.yspeed = 5 * Math.sin(angle);
+		this.x = width/2;
+		this.y = height/2;
+		let angle = random(-PI/4, PI/4);
+		this.xspeed = 5 * Math.cos(angle);
+		this.yspeed = 5 * Math.sin(angle);
 
-        if (random(1) < 0.5)
+		if (random(1) < 0.5)
 		{
-            this.xspeed *= -1;
-        }
-    }
+			this.xspeed *= -1;
+		}
+	}
 
-    edges()
+	edges()
 	{
-        if (this.y < 15 || this.y < 14 || this.y > height)
+		if (this.y < 15 || this.y < 14 || this.y > height)
 		{
-
-            this.yspeed *= -1;
-        }
+			this.yspeed *= -1;
+		}
 
 
 		if(this.y > height - 15)
@@ -187,8 +208,8 @@ class Ball
 
 		if (this.x < 15 || this.x < 14 || this.x > width)
 		{
-            this.xspeed *= -1;
-        }
+			this.xspeed *= -1;
+		}
 
 
 
@@ -196,15 +217,13 @@ class Ball
 		{
 			this.xspeed *= -1;
 		}
+	}
 
-
-    }
-
-    show()
+	show()
 	{
-        fill(255);
-        ellipse(this.x, this.y, this.r * 2);
-    }
+		fill(255);
+		ellipse(this.x, this.y, this.r * 2);
+	}
 
 	clicked()
 	{
@@ -214,67 +233,72 @@ class Ball
 		//if they are outside of the canvas then any click wont be counted as a miss
 		if (mouseX < width && mouseY < height)
 		{
-		//this if statement allows the system to see if the mouse is clicked within the radius of the ball
-		if(d < this.r)
-		{
-			success.play();
-			//this gives xspeed a random value 1 or 2
-			this.xspeedbool = Math.floor(Math.random() * 2) + 1;
-			if(this.xspeedbool == 1)
+			//this if statement allows the system to see if the mouse is clicked within the radius of the ball
+			if(d < this.r)
 			{
-				this.xspeed = random(-3, -10);
-			}
-			else
-			{
-				this.xspeed = random(3, 10);
-			}
-			//this gives yspeed a random value 1 or 2
-			this.yspeedbool = Math.floor(Math.random() * 2) + 1;
-			/*
-			this if statement determines what number was assigned to yspeed
-			in order to work out what speed to give the ball
-			if y speed is equal to 1 then it gives the ball a negative speed its movement is flipped
-			if its equal to anything else (2) then its assigned a positive value
-			*/
-			if(this.yspeedbool == 1)
-			{
-				this.yspeed = random(-3, -10);
-			}
-			else
-			{
-				this.yspeed = random(3, 10);
-			}
-			//this gets the score then adds one to it every time the ball is clicked
-			score = score + 1;
-
-			if(score > 2)
-			{
-				this.r = random(25, 75);
-				if(score > 4)
+				success.play();
+				//this gives xspeed a random value 1 or 2
+				this.xspeedbool = Math.floor(Math.random() * 2) + 1;
+				if(this.xspeedbool == 1)
 				{
-					this.xspeed = this.xspeed * 1.5;
-					this.yspeed = this.yspeed * 1.5;
+					this.xspeed = random(-3, -10);
+				}
+				else
+				{
+					this.xspeed = random(3, 10);
+				}
+				//this gives yspeed a random value 1 or 2
+				this.yspeedbool = Math.floor(Math.random() * 2) + 1;
+				/*
+				this if statement determines what number was assigned to yspeed
+				in order to work out what speed to give the ball
+				if y speed is equal to 1 then it gives the ball a negative speed its movement is flipped
+				if its equal to anything else (2) then its assigned a positive value
+				*/
+				if(this.yspeedbool == 1)
+				{
+					this.yspeed = random(-3, -10);
+				}
+				else
+				{
+					this.yspeed = random(3, 10);
+				}
+				//this gets the score then adds one to it every time the ball is clicked
+				score = score + 1;
+
+				if(score > 2)
+				{
+					this.r = random(25, 75);
+					if(score > 4)
+					{
+						this.xspeed = this.xspeed * 1.5;
+						this.yspeed = this.yspeed * 1.5;
+					}
 				}
 			}
-		}
-		else if(mouseX > width - 100 && mouseY > width - 100)
-		{
-			if(this.isMuted == false)
+			else if(mouseX > width - 100 && mouseY > height - 100)
 			{
-				theme.setVolume(0);
-				this.isMuted = true;
-				console.log(this.isMuted);
+				console.log("yes correct");
+				if(this.isMuted == false)
+				{
+					theme.setVolume(0);
+					fail.setVolume(0);
+					success.setVolume(0);
+					this.isMuted = true;
+					console.log(this.isMuted);
+				}
+				else
+				{
+					theme.setVolume(0.15);
+					fail.setVolume(0.75);
+					success.setVolume(0.5);
+					this.isMuted = false;
+					console.log(this.isMuted);
+				}
 			}
 			else
 			{
-				theme.setVolume(0.25);
-				this.isMuted = false;
-				console.log(this.isMuted);
-			}
-		}
-		else
-		{
-			fail.play();
+				fail.play();
 				// this is the same as the score but it takes one from the counter
 				miss = miss - 1;
 
@@ -283,11 +307,10 @@ class Ball
 				{
 					//this is a redirect to a page
 					window.location.replace("menu.html");
-          background(0);
+					background(0);
 				}
 			}
 		}
 	}
 }
-
 //https://codepen.io/rndm/pen/ozZzry
